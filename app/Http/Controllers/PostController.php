6928cac44b540ac2c0bc27fe408b62319b724cc5
ajavieralejandro <amorosijavier@gmail.com>
2022-09-28 11:23:21 -0300

    to lowercase views...

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -29,7 +29,7 @@ class PostController extends Controller
     public function create()
     {
         //
-        return view('admin.createPost');
+        return view('admin.createpost');
         
     }
 
