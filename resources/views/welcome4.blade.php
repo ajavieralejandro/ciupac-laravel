19f3aaedc2e407a154a6ab11acdbe76a0932949d
ajavieralejandro <amorosijavier@gmail.com>
2022-12-29 20:53:39 -0300

    article path

--- resources/views/welcome4.blade.php
+++ resources/views/welcome4.blade.php
@@ -429,7 +429,7 @@ Contacto</a>
              </h1>
              <ul class="mt-10 overflow-y-scroll ">
                @foreach($articles as $article)
-               <a href="{{$article->path}}">
+               <a href="{{asset($article->path)}}">
                <li class="hover:text-green-500 text-black">
        
                <svg class="inline-block w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg> {{$article->name}}
