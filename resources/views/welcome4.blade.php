8e4b56a56d3e1427eb25fbeaa5b6ba99045f7740
ajavieralejandro <amorosijavier@gmail.com>
2023-02-02 00:17:16 -0300

    section w and h full...

--- resources/views/welcome4.blade.php
+++ resources/views/welcome4.blade.php
@@ -327,7 +327,7 @@ Contacto</a>
 </div>
 <a name="news"></a>
 
-<section class="container   p-10 md:py-20 px-0 md:p-10 md:px-0">
+<section class="container h-full w-full   p-10 md:py-20 px-0 md:p-10 md:px-0">
 
 
 
