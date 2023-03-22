46494795177aa0d6d96f5403de65c0de349a96e0
ajavieralejandro <amorosijavier@gmail.com>
2023-03-22 13:43:16 -0300

    href actualizacion

--- resources/views/welcome4.blade.php
+++ resources/views/welcome4.blade.php
@@ -425,7 +425,7 @@ style="background-image:url({{asset($about->image_path.'/'.$about->image_name)}}
         <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Nosotros</h1>
         <p class="mt-6 text-lg leading-8 text-gray-600">{!!$about->body!!}</p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
-          <a href="#" class="rounded-md bg-blue-400 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Cargar Mediciones</a>
+          <a href="../ciupacperfiles/index.php"  class="rounded-md bg-blue-400 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Cargar Mediciones</a>
         </div>
       </div>
     </div>
