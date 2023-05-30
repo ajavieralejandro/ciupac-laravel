1f05a0356b126da44881bf982bf86485e66d8e99
ajavieralejandro <amorosijavier@gmail.com>
2023-05-30 11:23:56 -0300

    stations label ...

--- resources/views/welcome4.blade.php
+++ resources/views/welcome4.blade.php
@@ -482,7 +482,7 @@ style="background-image:url({{asset($about->image_path.'/'.$about->image_name)}}
         Eventos y tormentas
     </a>
     <a href="{{route('estaciones')}}"  class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
-        Estaciones
+        Estaciones Meteorológicas en vivo 
     </a>
    
    
