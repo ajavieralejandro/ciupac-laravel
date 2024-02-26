714c19a1583f2646d06ac243a7ea5f8638a2b2b3
ajavieralejandro <amorosijavier@gmail.com>
2024-02-26 19:10:10 -0300

    post controller changes...

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,7 +21,7 @@ class PostController extends Controller
     public function index()
     {
         //
-        $posts = Post::all()->orderBy('created_at','DESC')->paginate(15);
+        $posts = Post::all()->sortByDesc('created_at')->paginate(15);
         return view('admin.posts',['posts'=>$posts]);
         
     }
