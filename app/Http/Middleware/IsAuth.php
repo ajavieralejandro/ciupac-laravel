76275413a5a2cd2ca777dafba0bd62d58d92afcb
ajavieralejandro <amorosijavier@gmail.com>
2024-04-25 19:20:38 -0300

    Auth middleware for users

---
+++ app/Http/Middleware/IsAuth.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class IsAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = Auth::user();
+        if($user==null )
+            return redirect()->route('login');
+        if(!$user->is_auth)
+            return redirect()->route('users');
+        
+
+        return $next($request);
+    }
+}
