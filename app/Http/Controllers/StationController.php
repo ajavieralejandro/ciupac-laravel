6add4db63dfbcea7dc2a930c5b5c61e416ffc675
ajavieralejandro <amorosijavier@gmail.com>
2024-07-05 13:52:20 -0300

    bug fixed

--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -110,7 +110,6 @@ class StationController extends Controller
         $api_key = env('API_KEY');
         $api_token = env('API_TOKEN');
         $url = "https://api.ecowitt.net/api/v3/device/real_time?application_key=".$api_key."&api_key=".$api_token."&mac=".$mac."&call_back=all";
-        dd($url);
         $response = Http::get($url); 
         $data= $response->json();
         $data = $data['data'];
